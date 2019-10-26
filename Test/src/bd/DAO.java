aa37bf048a591bd86299f2c8e6f729861b0b461d
Julio Contreras <julio.unlp2010@gmail.com>
2019-10-26 18:06:08 -0300

    cambios

--- Test/src/bd/DAO.java
+++ Test/src/bd/DAO.java
@@ -1,17 +1,42 @@
 package bd;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DAO {
 	
 	
-	
+	public static Connection conectar() {
+		Connection con = null;
+		
+		String password = "root";
+		String usuario = "root";
+		String url = "jdbc:mysql://localhost:3306/ventas?user=" + usuario
+				+ "&password=" + password;
+		try {
+			con = DriverManager.getConnection(url);
+			if (con != null) {
+				System.out.println("Conectado");
+			}
+		} catch (SQLException e) {
+			System.out.println("No se pudo conectar a la base de datos");
+			e.printStackTrace();
+		}
+		return con;
+	}
 	
 
 	public static void main(String[] args) {
 		// Generar conexion con la base de datos . 
 
+	
+	conectar();
+	
 	}
 	
 	
 	
 
 }
+
+
